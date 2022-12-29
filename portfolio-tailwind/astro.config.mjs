96dd5b19a75cf022563b0a2a97e9b0b861a375de
upsatwal <UPSAtwal@users.noreply.github.com>
2022-12-29 23:47:16 +0530

    rebase

--- portfolio-tailwind/astro.config.mjs
+++ portfolio-tailwind/astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@astrojs/tailwind';
 
 //import LocomotiveScroll from 'locomotive-scroll';
 //import '../styles/locomotive-scroll.css';
+
+import VanillaTilt from 'vanilla-tilt';
+//import '../styles/locomotive-scroll.css';
 //const scroll = new LocomotiveScroll();
 
 // https://astro.build/config
